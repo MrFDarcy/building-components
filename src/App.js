0c72a7afb2be6f22abe17561acf3b76ce546c60c
Aditya Patil <patiladityadr@gmail.com>
2023-02-07 17:07:36 +0530

    Created a base

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import ProfileCard from "./ProfileCard";
+
+
+function App() {
+    return (
+        <div>
+            <h1>Personal Digital Assistants</h1>
+
+            <ProfileCard />
+
+        </div>
+
+    );
+
+}
+
+
+export default App;
