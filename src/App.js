6f127404d6bdd448232736ea85d727f8fbe95102
Aditya Patil <patiladityadr@gmail.com>
2023-02-07 17:20:54 +0530

    Added Images

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import ProfileCard from "./ProfileCard";
 
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
+
+
 
 function App() {
     return (
         <div>
             <h1>Personal Digital Assistants</h1>
 
-            <ProfileCard title="Alexa" handle="@alexa" />
-            <ProfileCard title="Cortana" handle="@cortana" />
-            <ProfileCard title="Siri" handle="@siri" />
+            <ProfileCard title="Alexa" handle="@alexa" image={AlexaImage} />
+            <ProfileCard title="Cortana" handle="@cortana" image={CortanaImage} />
+            <ProfileCard title="Siri" handle="@siri" image={SiriImage} />
 
         </div>
 
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,6 +1,7 @@
-function ProfileCard({ title, handle }) {
+function ProfileCard({ title, handle, image }) {
     return (
         <div>
+            <img src={image} alt={title} />
             <div>
                 Title : {title}
             </div>
